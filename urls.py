03dc7da8ffe2e6b30820b28e8439cb86940c75c3
apple <1729282897@qq.com>
2020-04-13 17:26:43 +0800

    use eureka

--- urls.py
+++ urls.py
@@ -2,12 +2,13 @@
 
 from libs.handler import ErrorHandler
 from libs.decorators import route
-from handlers import extensionCheck
+from handlers import extensionCheck, systemInfo
 
 handlers = []
 
 # add xxxx handlers
-handlers.extend(extensionCheck.handlers)   # app 版本相关功能
+handlers.extend(extensionCheck.handlers)  # app 版本相关功能
+handlers.extend(systemInfo.handlers)
 #
 # # add @route handlers
 handlers.extend(route.get_routes())
